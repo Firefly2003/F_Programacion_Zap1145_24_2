f43fffa02fb45993275c32e38992a15e928037f2
Firefly2003 <160138490+Firefly2003@users.noreply.github.com>
2024-05-06 08:34:29 -0600

    Antonio Rivera Mora
    asignacion de espacio para las practicas 6 y 7

---
+++ P006 Repaso Entrada y Salida de datos/P006 Repaso Entrada y Salida de datos.cpp
@@ -0,0 +1,9 @@
+// Trabajo en proceso
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ P007 Decisiones (IF-ELSE-SWITCH CASE)/P007 Decisiones (IF-ELSE-SWITCH CASE).cpp
@@ -0,0 +1,10 @@
+// Todavia no terminado
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
